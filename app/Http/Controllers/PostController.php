e971a34d27faad8f3bd8a14e5937e858b609540b
Md. Shad Chowdhury <tshad901@gmail.com>
2024-05-23 13:50:10 +0600

    implement post.index blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::all();
+
+        return view('posts.index', [ 'posts' => $posts]);
     }
 
     /**
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,22 @@
 <x-layout>
     <h1 class="title">Latest Posts</h1>
+
+    @foreach ($posts as $post)
+        <div class="card mb-3">
+            {{-- Title --}}
+            <p class="font-bold text-xl">{{ $post->title }}</p>
+
+            {{-- Author & Date --}}
+            <div class="text-xs mb-4">
+                <span class=""><i>Posted {{ $post->created_at->diffForHumans() }} by</i></span>
+                <a href="" class="text-blue-500 font-medium">Username</a>
+            </div>
+
+            {{-- Body --}}
+            <div class="text-sm">
+                <p>{{ Str::words($post->body, 40, '...') }}</p>
+            </div>
+
+        </div>
+    @endforeach
 </x-layout>
