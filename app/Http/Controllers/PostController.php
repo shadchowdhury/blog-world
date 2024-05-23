544c8b71bdeeee50597e928b31999f771a6996e3
Md. Shad Chowdhury <tshad901@gmail.com>
2024-05-23 14:22:59 +0600

    updated post controller and post.index blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
 
         return view('posts.index', [ 'posts' => $posts]);
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        Post::factory(15)->create();
+        Post::factory(2)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
